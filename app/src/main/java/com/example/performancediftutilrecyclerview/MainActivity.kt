403e449c762e0c2d12e43b233aea679353567356
makazemi <mary8shtr@gmail.com>
2019-10-07 09:21:03 +0330

    Initial commit

---
+++ app/src/main/java/com/example/performancediftutilrecyclerview/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.performancediftutilrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.performancediftutilrecyclerview.adapter.MyAdapter
+import com.example.performancediftutilrecyclerview.model.BlogPost
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),MyAdapter.Interaction {
+
+
+    val TAG="MainActivity"
+
+    private lateinit var blogAdapter:MyAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initRcy()
+    }
+
+    private fun initRcy(){
+        blogAdapter=MyAdapter(this)
+        recyclerView.apply {
+            layoutManager=LinearLayoutManager(context)
+            adapter=blogAdapter
+        }
+        blogAdapter.submitList(createList())
+    }
+
+    private fun createList():ArrayList<BlogPost>{
+        val list=ArrayList<BlogPost>()
+        val model1=BlogPost(1,"model1","body1","https://picsum.photos/200")
+        val model2=BlogPost(2,"model2","body2","https://picsum.photos/200")
+        val model3=BlogPost(3,"model3","body3","https://picsum.photos/200")
+        val model4=BlogPost(4,"model4","body4","https://picsum.photos/200")
+        list.add(model1)
+        list.add(model2)
+        list.add(model3)
+        list.add(model4)
+        return list
+    }
+
+    override fun onItemSelected(position: Int, item: BlogPost) {
+        Log.e(TAG,"item:$position")
+    }
+}
